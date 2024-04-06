33134aac647fa8a24c152815437ff116e4064a94
KevZ12 <135513743+KevZ12@users.noreply.github.com>
2024-04-06 08:40:18 +0700

    menambahkan class DataMahasiswa_1302223027

---
+++ DataMahasiswa_1302223027.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace modul7_kelompok_6
+{
+    internal class DataMahasiswa_1302223027
+    {
+        public string firstName;
+        public string lastName;
+        public string gender;
+        public int age;
+        public addressDetail address;
+        public List<collegeCourses_1302223027> courses;
+
+
+        public DataMahasiswa_1302223027()
+        {
+            courses = new List<collegeCourses_1302223027>();
+            address = new addressDetail();
+        }
+
+
+        public void ReadJSON()
+        {
+            string jsonContent = File.ReadAllText("D:\\praktikum kpl\\modul7_kelompok_6\\modul7_kelompok_6\\jurnal7_1_1302223027.json");
+            DataMahasiswa_1302223027 data = JsonConvert.DeserializeObject<DataMahasiswa_1302223027>(jsonContent);
+
+            firstName = data.firstName;
+            lastName = data.lastName;
+            gender = data.gender;
+            age = data.age;
+            address = data.address;
+            courses = data.courses;
+        }
+    }
+}
+
+public class addressDetail
+{
+    public string streetAddress;
+    public string city;
+    public string state;
+}
+
+public class collegeCourses_1302223027
+{
+    public string code;
+    public string name;
+}
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more inforda
+using modul7_kelompok_6;
+
+DataMahasiswa_1302223027 test1 = new DataMahasiswa_1302223027();
+
+test1.ReadJSON();
+
+Console.WriteLine(test1.firstName + " " + test1.lastName) ;
+Console.WriteLine(test1.gender);
+Console.WriteLine(test1.age);
+Console.WriteLine(test1.address.streetAddress+", "+test1.address.city+", "+test1.address.state) ;
+for (int i = 0 ; i < test1.courses.Count ; i++)
+{
+    Console.WriteLine(test1.courses[i].code);
+    Console.WriteLine(test1.courses[i].name);
+}  
